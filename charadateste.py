c76ea6b021f6cfc379b675dc6268c480afcaad22
LikeAJedi <Gabriel.correa2005@hotmail.com>
2017-06-16 14:06:45 -0300

    Add files via upload
    Codigo base do sistema de charadas( nao pronto)

---
+++ charadateste.py
@@ -0,0 +1,13 @@
+count = 3
+rc = input()
+while rc != 'A':
+    count = count-1
+    rc = input()
+    print(count)
+    if count == 0:
+        print('Fim de jogo')
+        break
+if rc == 'A':
+    print('Parabens')
+print('Arroz')
+
